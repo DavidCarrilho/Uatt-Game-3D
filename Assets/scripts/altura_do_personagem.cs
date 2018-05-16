418398c14dfb43b45cc41f451bb98bbc1f3ad74a
Luan David Carrilho <luan.david.oliver@gmail.com>
2018-05-16 14:42:57 -0300

    Main
    Main

---
+++ Assets/scripts/altura_do_personagem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class altura_do_personagem : MonoBehaviour {
+
+    public Transform personagem;
+    Text texto_da_interface;
+
+	// Use this for initialization
+	void Start () {
+        texto_da_interface = GetComponent<Text>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Debug.Log (personagem.position.y);
+        texto_da_interface.text = Mathf.RoundToInt (personagem.position.y).ToString();
+
+    }
+}
